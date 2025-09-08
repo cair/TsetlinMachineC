e6af5b38e2d78cb1ed4c424c24caf3e4ffaea6ff
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-09-08 11:56:55 +0200

    Update

--- TsetlinMachine.h
+++ TsetlinMachine.h
@@ -28,14 +28,11 @@ https://arxiv.org/abs/1804.01508
 #define THRESHOLD 15
 #define FEATURES 12
 #define CLAUSES 10
-#define NUMBER_OF_STATES 100
+#define NUMBER_OF_STATES 256
 #define BOOST_TRUE_POSITIVE_FEEDBACK 0
 
-#define PREDICT 1
-#define UPDATE 0
-
 struct TsetlinMachine { 
-	int ta_state[CLAUSES][FEATURES][2];
+	int ta_state[CLAUSES][FEATURES];
 
 	int clause_output[CLAUSES];
 
@@ -50,5 +47,5 @@ void tm_update(struct TsetlinMachine *tm, int Xi[], int target, float s);
 
 int tm_score(struct TsetlinMachine *tm, int Xi[]);
 
-int tm_get_state(struct TsetlinMachine *tm, int clause, int feature, int automaton_type);
+int tm_get_state(struct TsetlinMachine *tm, int clause, int feature);
 
