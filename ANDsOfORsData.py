a32eecc97953c9d19eb26b8518f0332b0eaed561
Ole-Christoffer Granmo <ole.granmo@uia.no>
2025-09-11 14:35:44 +0100

    Update

--- ANDsOfORsData.py
+++ ANDsOfORsData.py
@@ -16,7 +16,7 @@ for i in range(number_of_examples):
 	for v in range(number_of_variables):
 		k = np.random.randint(number_of_elements)
 		X_train[i, v*number_of_elements + k] = 1
-		Y_train[i] = np.logical_xor(Y_train[i], k >= (number_of_features // 2))
+		Y_train[i] = np.logical_xor(Y_train[i], k >= (number_of_elements// 2))
 
 Y_train = np.where(np.random.rand(number_of_examples) <= noise, 1-Y_train, Y_train) # Adds noise
 np.savetxt("ANDsOfORsTrainingData.txt", np.append(X_train, Y_train.reshape((number_of_examples, 1)), axis=1), fmt='%d')
@@ -28,5 +28,5 @@ for i in range(number_of_examples):
 	for v in range(number_of_variables):
 		k = np.random.randint(number_of_elements)
 		X_test[i, v*number_of_elements + k] = 1
-		Y_test[i] = np.logical_xor(Y_test[i], k >= (number_of_features // 2))
+		Y_test[i] = np.logical_xor(Y_test[i], k >= (number_of_elements // 2))
 np.savetxt("ANDsOfORsTestingData.txt", np.append(X_test, Y_test.reshape((number_of_examples, 1)), axis=1), fmt='%d')
