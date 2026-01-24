74f32c3275821a6fccddbc74980a595190bc487f
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-01-24 18:49:03 +0100

    Update

--- TsetlinMachine.h
+++ TsetlinMachine.h
@@ -25,11 +25,11 @@ https://arxiv.org/abs/1804.01508
 
 */
 
-#define THRESHOLD 15
+#define THRESHOLD 30
 #define FEATURES 12
 #define LITERALS (FEATURES*2)
 #define CLAUSES 2
-#define CLAUSE_COMPONENTS 5
+#define CLAUSE_COMPONENTS 10
 #define NUMBER_OF_STATES 100
 #define BOOST_TRUE_POSITIVE_FEEDBACK 0
 
