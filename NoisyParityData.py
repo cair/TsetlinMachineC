bf26cae695af3fa4ae8940d8c0664e9ec8a25d39
Ole-Christoffer Granmo <ole.granmo@uia.no>
2026-01-24 11:22:10 +0100

    Update

--- NoisyParityData.py
+++ NoisyParityData.py
@@ -4,6 +4,7 @@ import numpy as np
 noise = 0.4
 number_of_variables = 12
 number_of_examples = 20000
+parity_values = 2
 
 #X_train = np.zeros((number_of_examples, number_of_variables), dtype=np.uint32)
 
@@ -16,7 +17,7 @@ for i in range(number_of_examples):
 		X_train[i, j] = np.random.randint(2)
 		true_count += X_train[i, j]
 	#Y_train[i] = true_count % 2	
-	Y_train[i] = (X_train[i, 0] + X_train[i, 1]) % 2
+	Y_train[i] = (np.sum(X_train[i, 0:parity_values])) % 2
 
 Y_train = np.where(np.random.rand(number_of_examples) <= noise, 1-Y_train, Y_train) # Adds noise
 np.savetxt("NoisyParityTrainingData.txt", np.append(X_train, Y_train.reshape((number_of_examples, 1)), axis=1), fmt='%d')
@@ -30,6 +31,6 @@ for i in range(number_of_examples):
 		X_test[i, j] = np.random.randint(2)
 		true_count += X_test[i, j]
 	#Y_test[i] = true_count % 2	
-	Y_test[i] = (X_test[i, 0] + X_test[i, 1]) % 2	
+	Y_test[i] = (np.sum(X_test[i, 0:parity_values])) % 2	
 
 np.savetxt("NoisyParityTestingData.txt", np.append(X_test, Y_test.reshape((number_of_examples, 1)), axis=1), fmt='%d')
